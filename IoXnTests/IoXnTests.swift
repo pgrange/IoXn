b72ac8242a1891b05aff250bddfe4d1e6f395123
Pascal Grange <pascal@grange.nom.fr>
2025-02-14 18:59:03 +0100

    First test

--- IoXnTests/IoXnTests.swift
+++ IoXnTests/IoXnTests.swift
@@ -6,12 +6,27 @@
 //
 
 import Testing
+import Nimble
 @testable import IoXn
 
 struct IoXnTests {
 
     @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        let (workingStack, _) = step(
+            workingStack: [1, 2],
+            returnStack: [],
+            opcode: "ADD"
+        )
+        expect(workingStack[0]).to(equal(3))
     }
+}
 
+func step(
+    workingStack: [UInt8],
+    returnStack: [UInt8],
+    opcode: String
+) -> ([UInt8], [UInt8]) {
+    
+    let result = workingStack.suffix(2).reduce(0, +)
+    return (workingStack.dropLast(2) + [UInt8(result)], returnStack)
 }
